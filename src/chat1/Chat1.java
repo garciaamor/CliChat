75ffc93065add9c9c83ebc7fa826ccf74f406090
garciaamor <jgarciaamor@danielcastelao.org>
2017-02-13 11:00:44 +0100

    Cliente chat

--- src/chat1/Chat1.java
+++ src/chat1/Chat1.java
@@ -1,21 +1,22 @@
 
 package chat1;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+
+import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import javax.swing.JOptionPane;
 
 public class Chat1 {
 
+   public static int bucle=1;
+   public static String text;
+   public static String leer;
     
     public static void main(String[] args) throws InterruptedException, IOException {
-       String text;
+       
         
        
        
@@ -23,28 +24,16 @@ public class Chat1 {
             System.out.println("Creando socket cliente");
             Socket clienteSocket=new Socket("localhost",5555);
             System.out.println("Estableciendo la conexion");
-            BufferedReader is = new BufferedReader(new InputStreamReader(clienteSocket.getInputStream()));
-            BufferedWriter os = new BufferedWriter(new OutputStreamWriter(clienteSocket.getOutputStream()));
-            /*int numCompr=0;
-            while (numCompr==0){
-             
-           
-            }*/
-            System.out.println("Enviando mensaje");
+            DataInputStream is = new DataInputStream(clienteSocket.getInputStream());
+            PrintStream os = new PrintStream(clienteSocket.getOutputStream());
+            
             
             text = JOptionPane.showInputDialog("(C)Escribe un mensaje");
            
             System.out.println(text);
+                        
             
             
-            os.write(text);
-            os.flush();
-            
-              
-       
-            clienteSocket.close();
-            
-
             }catch (IOException e) {
                     e.printStackTrace();
             }
